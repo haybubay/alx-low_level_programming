af99594cd02024063558767e55db42f399f30006
haybubay <udenzeebube07@gmail.com>
2022-09-08 17:29:14 -0700

    printf

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+* main - print the script in the put function
+*
+*description: using the main function
+* this program prints "programming is like building a multilingual puzzle
+* Return: 0
+*/
+init main(void)
+{
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+/**
+ * main - print the string in the put function
+ *
+ * Description: using the main function
+ * this program prints "programming is like building a multilingual puzzle"
+ * Return: 0
+ */
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a place of art,\n");
+	return (0)
+}
