2755a4b931c9886ef782b773defd1928e0779d68
haybubay <udenzeebube07@gmail.com>
2022-09-09 17:36:39 -0700

    I sometimes suffer from insomnia

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 /**
  *
- * * main - print if the number is postive, zero, or negative
+ * main - print if the number is postive, zero, or negative
  *
- * *
+ * 
  *
- * * Description: using the main function
+ * Description: using the main function
  *
- * * this program prints "Programming is positive, zero, or negative
+ * this program prints "Programming is positive, zero, or negative
  *
- * * Return: 0
+  * Return: 0
  *
  * */
 int main(void)
